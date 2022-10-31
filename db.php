e54b473ee2081bd56aaf219bed082d2134dff6f5
Sriram <sri761177@gmail.com>
2022-10-31 20:26:55 +0530

    Connect DB to AWS RDS

--- db.php
+++ db.php
@@ -1,7 +1,8 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost","guvi_project","guvipass","guvi_db");
+    $con = mysqli_connect("database-guvi.ck2rgsp30sfg.ap-south-1.rds.amazonaws.com","admin","guvipass","guvi_db");
+
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
